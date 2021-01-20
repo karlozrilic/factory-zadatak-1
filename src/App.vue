99512f8b1589151eb0cf4cfeeb89b0857746ffe2
karlozrilic <kazrile@gmail.com>
2021-01-20 16:27:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header>
+      <div class="top-navigation">
+        <div class="top-navigation-content">
+          <div class="top-navigation-brand">
+            <div class="top-logo">
+              <img src="./assets/svg/kenaz.svg" />
+            </div>
+            <p>Kenaz</p>
+          </div>
+          <div class="top-navigation-links">
+            <ul>
+              <li><a href="#">Media</a></li>
+              <li><a href="#">Marketing</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <button class="top-navigation-search"><img src="./assets/svg/search.svg" /></button>
+          </div>
+        </div>
+      </div>
+      <div class="lower-navigation">
+        <div class="lower-navigation-content">
+          <ul>
+            <li><a href="/" id="news">NEWS</a></li>
+            <li><a href="/about" id="business">BUSINESS</a></li>
+            <li><a href="/about" id="sport">SPORT</a></li>
+            <li><a href="/about" id="life">LIFE</a></li>
+            <li><a href="/about" id="tech">TECH</a></li>
+            <li><a href="/about" id="travel">TRAVEL</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<style lang="sass">
+@import "assets/styles/variables"
+@import "assets/styles/style"
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,272 @@
+<template>
+  <div class="main-content">
+    <div class="banner">
+      <p>banner</p>
+      <p>940x120</p>
+    </div>
+    <div class="slider">
+      <div>
+        <img src="../assets/svg/image.svg" />
+        <div class="image-info">
+          <div class="article-info">
+            <div class="article-date">August 26, 2013</div>
+            <div class="article-comments"><img src="../assets/svg/comment.svg" /> 22 Comments</div>
+          </div>
+          <h1>'Margot' breathlessly reimagines Anne Frank's sister</h1>
+          <a class="article-link" href="#">Read article</a>
+        </div>
+      </div>
+      <div>
+        <img src="../assets/svg/image.svg" />
+        <div class="image-info">
+          <div class="article-info">
+            <div class="article-date">August 26, 2014</div>
+            <div class="article-comments"><img src="../assets/svg/comment.svg" /> 14 Comments</div>
+          </div>
+          <h1>'Margot' breathlessly reimagines Anne Frank's sister</h1>
+          <a class="article-link" href="#">Read article</a>
+        </div>
+      </div>
+      <div>
+        <img src="../assets/svg/image.svg" />
+        <div class="image-info">
+          <div class="article-info">
+            <div class="article-date">August 26, 2015</div>
+            <div class="article-comments"><img src="../assets/svg/comment.svg" /> 5 Comments</div>
+          </div>
+          <h1>'Margot' breathlessly reimagines Anne Frank's sister</h1>
+          <a class="article-link" href="#">Read article</a>
+        </div>
+      </div>
+    </div>
+    <div class="center-articles">
+      <div class="main-articles">
+        <div class="news">
+          <div class="main-article-top">
+            <h1>News</h1>
+            <a href="#">See all</a>
+          </div>
+          <div class="articles">
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+                <p>14</p>
+              </div>
+              <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+                <p>22</p>
+              </div>
+              <p class="article-title">NASA releases portrait of a planet waving at Saturn</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+                <p>5</p>
+              </div>
+              <p class="article-title">Patriotsvv make cuts ... and Tim Tebow survives (so far)</p>
+            </a>
+          </div>
+        </div>
+        <div class="sport">
+          <div class="main-article-top">
+            <h1>Sport</h1>
+            <a href="#">See all</a>
+          </div>
+          <div class="articles">
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">NASA releases portrait of a planet waving at Saturn</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">Patriotsvv make cuts ... and Tim Tebow survives (so far)</p>
+            </a>
+          </div>
+        </div>
+        <div class="banner">
+          <p>banner</p>
+          <p>620x120</p>
+        </div>
+        <div class="business">
+          <div class="main-article-top">
+            <h1>Business</h1>
+            <a href="#">See all</a>
+          </div>
+          <div class="articles">
+            <a class="small-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-side">
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </div>
+            </a>
+            <a class="small-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-side">
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </div>
+            </a>
+            <a class="small-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-side">
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </div>
+            </a>
+            <a class="small-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-side">
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="banner">
+          <p>banner</p>
+          <p>620x120</p>
+        </div>
+        <div class="news-carousel">
+          <div class="main-article-top">
+            <h1>News Carousel</h1>
+            <div class="arrows">
+              <img class='a-left control-c prev slick-prev' id="news-carousel-prev" src='../assets/svg/prevArrowNewsCarousel.svg'>
+              <img class='a-right control-c next slick-next' id="news-carousel-next" src='../assets/svg/nextArrowNewsCarousel.svg'>
+            </div>
+          </div>
+          <div class="articles news-carousel-slider">
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">NASA releases portrait of a planet waving at Saturn</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">NASA releases portrait of a planet waving at Saturn</p>
+            </a>
+            <a class="medium-article" href="#">
+              <img src="../assets/svg/article-image.svg" />
+              <div class="article-date-and-views">
+                <p>August 26, 2013</p>
+              </div>
+              <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+            </a>
+          </div>
+        </div>
+        <div class="single-carousels">
+          <div class="editorials">
+            <div class="main-article-top">
+              <h1>Editorials</h1>
+              <div class="arrows">
+                <img class='a-left control-c prev slick-prev' id="editorials-prev" src='../assets/svg/prevArrowNewsCarousel.svg'>
+                <img class='a-right control-c next slick-next' id="editorials-next" src='../assets/svg/nextArrowNewsCarousel.svg'>
+              </div>
+            </div>
+            <div class="articles editorials-slider">
+              <a class="medium-article" href="#">
+                <img src="../assets/svg/article-image.svg" />
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </a>
+              <a class="medium-article" href="#">
+                <img src="../assets/svg/article-image.svg" />
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </a>
+              <a class="medium-article" href="#">
+                <img src="../assets/svg/article-image.svg" />
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </a>
+            </div>
+          </div>
+          <div class="local-news">
+            <div class="main-article-top">
+              <h1>Local News</h1>
+              <div class="arrows">
+                <img class='a-left control-c prev slick-prev' id="local-news-prev" src='../assets/svg/prevArrowNewsCarousel.svg'>
+                <img class='a-right control-c next slick-next' id="local-news-next" src='../assets/svg/nextArrowNewsCarousel.svg'>
+              </div>
+            </div>
+            <div class="articles local-news-slider">
+              <a class="medium-article" href="#">
+                <img src="../assets/svg/article-image.svg" />
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </a>
+              <a class="medium-article" href="#">
+                <img src="../assets/svg/article-image.svg" />
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </a>
+              <a class="medium-article" href="#">
+                <img src="../assets/svg/article-image.svg" />
+                <div class="article-date-and-views">
+                  <p>August 26, 2013</p>
+                </div>
+                <p class="article-title">For Obama, MLK's shadow looms large ahead of speech</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <aside>
+
+      </aside>
+    </div>
+    <div class="banner">
+      <p>banner</p>
+      <p>940x120</p>
+    </div>
+  </div>
+</template>
