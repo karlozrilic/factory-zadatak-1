d7c21c41f83d2da6fd6b2248bdc797578e085199
karlozrilic <kazrile@gmail.com>
2021-01-26 09:48:56 +0100

    finished Single page and separated aside to separate file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -260,123 +260,7 @@
           </div>
         </div>
       </div>
-      <aside>
-        <div class="aside-articles">
-          <div class="aside-articles-links">
-            <ul>
-              <li><a href="#" class="active">Popular</a></li>
-              <li><a href="#">Top Rated</a></li>
-              <li><a href="#">Comments</a></li>
-            </ul>
-          </div>
-          <a class="article" href="/single">
-            <div class="article-content">
-              <div class="article-comments"><img src="../assets/svg/greyComment.svg" /> 5</div>
-              <div class="article-info">
-                <div class="article-date">August 26, 2014</div>
-                <p class="article-title">Palestinian call off peace talks after clash</p>
-              </div>
-            </div>
-            <div class="article-image">
-              <img src="../assets/svg/asideArticle.svg" />
-            </div>
-          </a>
-          <a class="article" href="/single">
-            <div class="article-content">
-              <div class="article-comments"><img src="../assets/svg/greyComment.svg" /> 5</div>
-              <div class="article-info">
-                <div class="article-date">August 26, 2014</div>
-                <p class="article-title">Palestinian call off peace talks after clash</p>
-              </div>
-            </div>
-            <div class="article-image">
-              <img src="../assets/svg/asideArticle.svg" />
-            </div>
-          </a>
-          <a class="article" href="/single">
-            <div class="article-content">
-              <div class="article-comments"><img src="../assets/svg/greyComment.svg" /> 5</div>
-              <div class="article-info">
-                <div class="article-date">August 26, 2014</div>
-                <p class="article-title">Palestinian call off peace talks after clash</p>
-              </div>
-            </div>
-            <div class="article-image">
-              <img src="../assets/svg/asideArticle.svg" />
-            </div>
-          </a>
-          <a class="article" href="/single">
-            <div class="article-content">
-              <div class="article-comments"><img src="../assets/svg/greyComment.svg" /> 5</div>
-              <div class="article-info">
-                <div class="article-date">August 26, 2014</div>
-                <p class="article-title">Palestinian call off peace talks after clash</p>
-              </div>
-            </div>
-            <div class="article-image">
-              <img src="../assets/svg/asideArticle.svg" />
-            </div>
-          </a>
-          <a class="article" href="/single">
-            <div class="article-content">
-              <div class="article-comments"><img src="../assets/svg/greyComment.svg" /> 5</div>
-              <div class="article-info">
-                <div class="article-date">August 26, 2014</div>
-                <p class="article-title">Palestinian call off peace talks after clash</p>
-              </div>
-            </div>
-            <div class="article-image">
-              <img src="../assets/svg/asideArticle.svg" />
-            </div>
-          </a>
-        </div>
-        <div class="aside-social">
-          <div class="aside-social-title">Social</div>
-          <div class="social-networks">
-            <a class="social-network" href="/single">
-              <div class="social-icon"><img src="../assets/svg/social/facebook.svg" /></div>
-              <div class="social-text">Like</div>
-              <div class="social-number">25041 Fans</div>
-            </a>
-            <a class="social-network" href="/single">
-              <div class="social-icon"><img src="../assets/svg/social/twitter.svg" /></div>
-              <div class="social-text">Follow</div>
-              <div class="social-number">25041 Followers</div>
-            </a>
-            <a class="social-network" href="/single">
-              <div class="social-icon"><img src="../assets/svg/social/youtube.svg" /></div>
-              <div class="social-text">Subscribe</div>
-              <div class="social-number">941 Subscribers</div>
-            </a>
-          </div>
-        </div>
-        <div class="aside-tv">
-          <div class="aside-tv-title">Kneaz TV</div>
-          <a class="aside-tv-link" href="/single">
-            <div class="aside-tv-video">
-              <img class="aside-tv-video-image" src="../assets/svg/video.svg" />
-              <img class="aside-tv-video-button" src="../assets/svg/videoPlay.svg" />
-            </div>
-            <div class="aside-tv-text">
-              <div class="aside-tv-date">August 26, 2013</div>
-              <div class="aside-tv-article-title">NASA releases portrait of a planet waving at Saturn</div>
-            </div>
-          </a>
-        </div>
-        <div class="aside-banners">
-          <div class="aside-banner">
-            <p>banner</p>
-            <p>300x250</p>
-          </div>
-          <div class="aside-banner">
-            <p>banner</p>
-            <p>300x250</p>
-          </div>
-        </div>
-        <div class="aside-facebook">
-            <img src="../assets/svg/asideFacebook.svg" />
-        </div>
-      </aside>
+      <Aside />
     </div>
     <div class="banner">
       <p>banner</p>
@@ -435,3 +319,13 @@
     </div>
   </div>
 </template>
+
+<script>
+import Aside from './Aside.vue';
+
+export default {
+    components: {
+        Aside
+    }
+}
+</script>
--- src/views/Single.vue
+++ src/views/Single.vue
@@ -13,5 +13,141 @@
                 </div>
             </div>
         </div>
+        <div class="center-articles">
+            <div class="main-articles">
+                <div class="article">
+                    <div class="text">
+                        Vestibulum id ligula porta felis 
+                        euismod semper. Morbi leo risus, 
+                        porta ac consectetur ac, vestibulum at eros. 
+                        Aenean lacinia bibendum nulla sed consectetur. 
+                        Sed posuere consectetur est at lobortis. Donec sed odio dui. 
+                        Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, 
+                        dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. 
+                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. 
+                        Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec 
+                        sed odio dui. Nulla vitae elit libero, a pharetra augue. Curabitur 
+                        blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. 
+                        Nullam quis risus eget urna mollis ornare vel eu leo.
+                    </div>
+                    <img src="../assets/svg/singlePage/articleImage.svg" />
+                    <div class="text">
+                        Molestias ultricies, ante quam urna ut volutpat, egestas dolor 
+                        dui, nec hac ultrices nulla non netus. Placerat vehicula donec 
+                        non suscipit egestas, augue vel suspendisse. Et felis venenatis 
+                        blandit sed est ultrices, adipiscing urna, at aliquam nullam 
+                        facilisis aliquet sapien, eget duis consectetuer tristique nunc 
+                        vitae erat, mi purus nisl lorem. Ac magna lobortis non, vulputate 
+                        vitae viverra. Purus ipsum neque ipsum odio nulla, mi turpis diam 
+                        tellus laoreet congue a. Rhoncus maecenas, sit suspendisse, 
+                        condimentum purus convallis dui hendrerit. Purus ipsum neque ipsum 
+                        odio nulla, mi turpis diam tellus laoreet congue. 
+                    </div>
+                    <img class="social" src="../assets/svg/singlePage/social.svg" />
+                </div>
+                <div class="banner">
+                    <p>banner</p>
+                    <p>620x120</p>
+                </div>
+                <div class="author-section">
+                    <div class="title">About the Author</div>
+                    <div class="author-content">
+                        <img src="../assets/svg/singlePage/authorImage.svg" />
+                        <div class="about-author">
+                            Molestias ultricies, ante quam urna ut volutpat, 
+                            egestas dolor dui, nec hac ultrices nulla non netus. 
+                            Placerat vehicula donec non suscipit egestas, augue vel 
+                            suspendisse. Et felis venenatis blandit sed est ultrices, 
+                            adipiscing urna, at aliquam nullam facilisis aliquet sapien, 
+                            eget duis consectetuer tristique nunc vitae erat, mi purus nisl lorem. 
+                        </div>
+                    </div>
+                </div>
+                <div class="comments-section">
+                    <div class="title">Comments</div>
+                    <div class="comments">
+                        <div class="comment">
+                            <img src="../assets/svg/singlePage/profilePic.svg" />
+                            <div class="comment-body">
+                                <div class="top">
+                                    <div class="user-name">Matt Adams</div>
+                                    <div class="date">Jan 2nd, 2012 2:35 pm</div>
+                                </div>
+                                <div class="text">
+                                    Molestias ultricies, ante quam urna ut volutpat, 
+                                    egestas dolor dui, nec hac ultrices nulla non netus. 
+                                    Placerat vehicula donec non suscipit egestas, augue vel 
+                                    suspendisse. Et felis venenatis blandit sed est ultrices, 
+                                    adipiscing urna.
+                                </div>
+                                <a class="comment-reply" href="#">Reply</a>
+                            </div>
+                        </div>
+                        <div class="comment">
+                            <img src="../assets/svg/singlePage/profilePic.svg" />
+                            <div class="comment-body">
+                                <div class="top">
+                                    <div class="user-name">John Toews</div>
+                                    <div class="date">Jan 2nd, 2012 2:35 pm</div>
+                                </div>
+                                <div class="text">
+                                    Molestias ultricies, ante quam urna ut volutpat, 
+                                    egestas dolor dui, nec hac ultrices nulla non netus. 
+                                    Placerat vehicula donec non suscipit egestas, augue vel 
+                                    suspendisse. Et felis venenatis blandit sed est ultrices, 
+                                    adipiscing urna.
+                                </div>
+                                <a class="comment-reply" href="#">Reply</a>
+                            </div>
+                        </div>
+                        <div class="comment">
+                            <img src="../assets/svg/singlePage/profilePic.svg" />
+                            <div class="comment-body">
+                                <div class="top">
+                                    <div class="user-name">Andrew Enns</div>
+                                    <div class="date">Jan 2nd, 2012 2:35 pm</div>
+                                </div>
+                                <div class="text">
+                                    Molestias ultricies, ante quam urna ut volutpat, 
+                                    egestas dolor dui, nec hac ultrices nulla non netus. 
+                                    Placerat vehicula donec non suscipit egestas, augue vel 
+                                    suspendisse. Et felis venenatis blandit sed est ultrices, 
+                                    adipiscing urna.
+                                </div>
+                                <a class="comment-reply" href="#">Reply</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="title">Add Your Comment</div>
+                    <div class="add-comment">
+                        <div class="text">
+                            Molestias ultricies, ante quam urna ut volutpat, 
+                            egestas dolor dui, nec hac ultrices nulla non netus. 
+                            Placerat vehicula donec non suscipit egestas, augue vel 
+                            suspendisse. Et felis venenatis blandit sed est ultrices, 
+                            adipiscing urna.
+                        </div>
+                        <form class="add-comment-form" method="POST" action="/">
+                            <input type="text" placeholder="Name" />
+                            <input type="email" placeholder="Email Address" />
+                            <textarea rows="8" cols="50" placeholder="Comment">
+                            </textarea>
+                            <button class="add-comment-button">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <Aside />
+        </div>
     </div>
 </template>
+
+<script>
+import Aside from './Aside.vue';
+
+export default {
+    components: {
+        Aside
+    }
+}
+</script>
